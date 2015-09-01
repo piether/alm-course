69ddbf56df37c9bdc1fad48d4739b75bcc6784bf
Pieter Herroelen <pieter.herroelen@gmail.com>
2015-09-02 00:12:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# alm-course [![Build Status](https://secure.travis-ci.org/piether/alm-course.png?branch=master)](http://travis-ci.org/piether/alm-course)
+
+Continuous integration server:
+https://travis-ci.org/piether/alm-course/branches
+
+Installatie
+-----------
+- installeer Node.js https://nodejs.org/download/
+- installeer GitHub Desktop: https://desktop.github.com/
+- clone deze repository
+- run 'npm install' in de root van de project directory
+- run 'npm install -g grunt-cli'
+
+Lokaal builden
+--------------
+
+   grunt
---
+++ test/alm-course_test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var alm_course = require('../lib/alm-course.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['welcomeMessage'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'all tests': function(test) {
+    test.expect(2);
+    // tests here
+    test.equal(alm_course.sum(1,2), 3, '1+2 should be 3.');
+    test.equal(alm_course.sum(1), 1, 'sum of 1 and nothing should be 1.');
+    test.done();
+  },
+};
